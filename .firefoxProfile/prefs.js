076c17bac98c4b7b439f63b2526d164b947e0810
Sheansuke <suerojean@gmail.com>
2019-03-18 14:47:02 -0400

    Trabajo completado

---
+++ .firefoxProfile/prefs.js
@@ -0,0 +1,5 @@
+
+user_pref("browser.shell.checkDefaultBrowser", false);
+user_pref("devtools.chrome.enabled", true);
+user_pref("devtools.debugger.prompt-connection", false);
+user_pref("devtools.debugger.remote-enabled", true);
